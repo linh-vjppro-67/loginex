38f3e7a0e76058e9dc9a35c335f1c8b40165aedb
Linh Pham <linhpham@192.168.4.182>
2025-12-25 16:30:05 +0700

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/loginex">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Services />} />
