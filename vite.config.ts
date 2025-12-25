c3c9062ede85ad859840ca02d8a4dba3f8d3d41d
Linh Pham <linhpham@192.168.4.182>
2025-12-25 15:58:35 +0700

    init

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/loginex/",   // 👈 THÊM DÒNG NÀY (TÊN REPO)
   server: {
     host: "::",
     port: 8080,
