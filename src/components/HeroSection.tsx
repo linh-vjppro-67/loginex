49cfadab2e7274b7e590738431dae4c101967563
Linh Pham <linhpham@192.168.4.182>
2025-12-25 17:04:40 +0700

    init

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -144,13 +144,6 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-
-      {/* Scroll indicator */}
-      <div className="absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 rounded-full border-2 border-primary/50 flex items-start justify-center p-2">
-          <div className="w-1 h-2 bg-primary rounded-full" />
-        </div>
-      </div>
     </section>
   );
 };
